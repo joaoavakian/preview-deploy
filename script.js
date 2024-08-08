ae635f801593e89b0c269ab67410e26a737df1b1
joaoavakian <joaoavakian7@gmail.com>
2024-08-08 17:34:13 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+});
